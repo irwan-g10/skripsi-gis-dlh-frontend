d7fe499d3b9ec15ab55fe7cc372375b89fa3e189
irwan-g10 <irwangumilar111@gmail.com>
2024-09-10 00:07:45 +0700

    tambah route admin side

--- src/components/AdminSideContent.js
+++ src/components/AdminSideContent.js
@@ -1,28 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AdminSideContent() {
   return (
     <ul class="list-group">
       <li class="list-group-item">user</li>
-      <li class="list-group-item active" aria-current="true">
-        <i class="bi bi-geo-alt-fill"></i> Titik TPA
-      </li>
-      <li class="list-group-item">
-        <i class="bi bi-calendar-day"></i> Jadwal Pengangkutan
-      </li>
-      <li class="list-group-item">
-        <i class="bi bi-building-fill"></i> UPT
-      </li>
+
+      <Link to="/titik-tpa">
+        <li class="list-group-item active" aria-current="true">
+          <i class="bi bi-geo-alt-fill"></i> Titik TPA
+        </li>
+      </Link>
+
+      <Link to="/jadwal-pengangkutan">
+        <li class="list-group-item">
+          <i class="bi bi-calendar-day"></i> Jadwal Pengangkutan
+        </li>
+      </Link>
+      <Link to="/upt">
+        <li class="list-group-item">
+          <i class="bi bi-building-fill"></i> UPT
+        </li>
+      </Link>
       <li class="list-group-item">
         <i class="bi bi-book-fill"> </i>
         Laporan
         <i class="bi bi-chevron-down"></i>
       </li>
-      <li class="list-group-item text-end">Pengangkutan</li>
-      <li class="list-group-item text-end">Pengaduan</li>
-      <li class="list-group-item">
-        <i class="bi bi-person-fill"></i> Pengguna
-      </li>
+      <Link to="/laporan-pengangkutan">
+        <li class="list-group-item text-end">Pengangkutan</li>
+      </Link>
+      <Link to="/laporan-pengaduan">
+        <li class="list-group-item text-end">Pengaduan</li>
+      </Link>
+      <Link to="/pengguna">
+        <li class="list-group-item">
+          <i class="bi bi-person-fill"></i> Pengguna
+        </li>
+      </Link>
       <li class="list-group-item">
         <br></br>
         <br></br>
