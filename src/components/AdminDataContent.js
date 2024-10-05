ac738954d52ab55416798ad3cefe6f84bd71dc55
irwan-g10 <irwangumilar111@gmail.com>
2024-10-05 15:55:42 +0700

    delete unused file

--- src/components/AdminDataContent.js
+++ src/components/AdminDataContent.js
@@ -4,9 +4,6 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import AdminTable from "./AdminTable";
-import AdminPagination from "./AdminPagination";
-import AdminOptionBar from "./AdminOptionBar";
-import { Route, Routes } from "react-router-dom";
 import AdminMaps from "./AdminMaps";
 
 function AdminDataContent({ title }) {
@@ -27,13 +24,58 @@ function AdminDataContent({ title }) {
             </div>
           </div>
         </div>
-        <AdminOptionBar />
+        <div className="AdminOptionBar">
+          <div class="container text-center mb-5">
+            <div class="row">
+              <div class="col ">
+                <div
+                  class="btn-group"
+                  role="group"
+                  aria-label="Basic radio toggle button group"
+                >
+                  <input
+                    type="radio"
+                    class="btn-check"
+                    name="btnradio"
+                    id="btnradio1"
+                    autocomplete="off"
+                    checked
+                  />
+                  <label class="btn btn-outline-secondary" for="btnradio1">
+                    Radio 1
+                  </label>
 
-        {/* <Routes>
-          <Route path="/titik-tpa-table" element={<AdminTable />}></Route>
-          <Route path="/titik-tpa-maps" element={<AdminMaps />}></Route>
-        </Routes> */}
+                  <input
+                    type="radio"
+                    class="btn-check"
+                    name="btnradio"
+                    id="btnradio2"
+                    autocomplete="off"
+                  />
+                  <label class="btn btn-outline-secondary" for="btnradio2">
+                    Radio 2
+                  </label>
+                </div>
+              </div>
+              <div class="col">
+                <form class="d-flex" role="search">
+                  <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button class="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <AdminTable />
+        <AdminMaps />
       </div>
     </div>
   );
