d518992ddfb89fa97ecb0004c525f9bc1bec5087
irwan-g10 <irwangumilar111@gmail.com>
2024-10-10 18:37:15 +0700

    update

--- src/components/pengangkut/pengaduan/ListPengaduan.js
+++ src/components/pengangkut/pengaduan/ListPengaduan.js
@@ -6,8 +6,8 @@ function ListPengaduan({ data }) {
       {data.map((item) => {
         return (
           <div className="row border rounded p-3 shadow m-5" key={item.id}>
-            <div className="col-3">
-              <img src="/images/user.png" width="150" alt="..." />{" "}
+            <div className="col-3 justify-content-center align-items-center d-flex">
+              <img src="/images/user.png" width="100" alt="..." />{" "}
             </div>
             <div className="col">
               <table className="table">
