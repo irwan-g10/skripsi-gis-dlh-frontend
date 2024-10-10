763a5d950069301a9902e94c42ef094a70e6ffe9
irwan-g10 <irwangumilar111@gmail.com>
2024-10-10 18:22:42 +0700

    update list pengaduan

---
+++ src/components/pengangkut/pengaduan/ListPengaduan.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function ListPengaduan({ data }) {
+  return (
+    <div className="ListPengaduan">
+      {data.map((item) => {
+        return (
+          <div className="row border rounded p-3 shadow m-5" key={item.id}>
+            <div className="col-3">
+              <img src="/images/user.png" width="150" alt="..." />{" "}
+            </div>
+            <div className="col">
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th scope="row">Pelapor</th>
+                    <td className="text-end">{item.nama}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Lokasi</th>
+                    <td className="text-end">{item.lokasi_kejadian}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">jarak</th>
+                    <td className="text-end">3 km</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Tanggal Pengaduan</th>
+                    <td className="text-end">10 Oktober 2020, 10.59</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ListPengaduan;
--- src/components/pengangkut/pengaduan/pengaduan.js
+++ src/components/pengangkut/pengaduan/pengaduan.js
@@ -1,9 +1,28 @@
+import axios from "axios";
 import React from "react";
+import ListPengaduan from "./ListPengaduan";
 
 function Pengaduan() {
+  const [data, setData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/laporan-pengaduan`)
+      .then((response) => {
+        setData(response.data.result);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }, []);
+
   return (
     <div className="Pengaduan">
+      {/* {console.log(data)} */}
       <h1>this is Pengaduan</h1>
+      {isLoading ? true : <ListPengaduan data={data} />}
     </div>
   );
 }
