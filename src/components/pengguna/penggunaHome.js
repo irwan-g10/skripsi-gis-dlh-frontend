a93cbaf6dc49b44c32674b398d33a551d34047d4
irwan-g10 <irwangumilar111@gmail.com>
2025-01-17 07:18:26 +0700

    update tampilan

--- src/components/pengguna/penggunaHome.js
+++ src/components/pengguna/penggunaHome.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import InformasiGeografis from "./informasi-geografis/InformasiGeografis";
+import JadwalPengangkutan from "./jadwal-pengangkutan/JadwalPengangkutan";
+import LaporPengaduanSampah from "./lapor-pengaduan/LaporPengaduanSampah";
 
 function PenggunaHome() {
   return (
@@ -21,11 +24,11 @@ function PenggunaHome() {
           <div className="row">
             <div className="col m-2">
               <a href="/informasi-geografis">
-                <div className="row border rounded p-2 shadow ">
-                  <div className="col-3 ">
-                    <img src="/images/user.png" width="100" alt="..." />
+                <div className="row border rounded p-2 shadow  px-5">
+                  <div className="col-3 my-3">
+                    <i className="bi bi-map fs-1"></i>
                   </div>
-                  <div className="col  justify-content-center align-items-center d-flex ms-5">
+                  <div className="col  justify-content-center align-items-center text-end d-flex ms-5">
                     <h5>Informasi Geografis</h5>
                   </div>
                 </div>
@@ -33,11 +36,11 @@ function PenggunaHome() {
             </div>
             <div className="col m-2">
               <a href="/jadwal-pengangkutan">
-                <div className="row border rounded p-2 shadow ">
-                  <div className="col-3 ">
-                    <img src="/images/user.png" width="100" alt="..." />
+                <div className="row border rounded p-2 shadow px-3">
+                  <div className="col-3 my-3 ">
+                    <i className="bi bi-card-checklist fs-1"></i>
                   </div>
-                  <div className="col  justify-content-center align-items-center d-flex ms-5">
+                  <div className="col  justify-content-center text-end align-items-center d-flex ms-5">
                     <h4>Jadwal Pengangkutan</h4>
                   </div>
                 </div>
@@ -45,11 +48,11 @@ function PenggunaHome() {
             </div>
             <div className="col m-2">
               <a href="/lapor-pengaduan">
-                <div className="row border rounded p-2 shadow ">
-                  <div className="col-3 ">
-                    <img src="/images/user.png" width="100" alt="..." />
+                <div className="row border rounded p-2 shadow px-2">
+                  <div className="col-3 my-3">
+                    <i className="bi bi-megaphone fs-1"></i>
                   </div>
-                  <div className="col  justify-content-center align-items-center d-flex ms-5">
+                  <div className="col  justify-content-center text-end align-items-center d-flex ms-5">
                     <h4>Lapor Pengaduan Sampah</h4>
                   </div>
                 </div>
@@ -58,6 +61,20 @@ function PenggunaHome() {
           </div>
         </div>
       </div>
+
+      <div className=" border rounded p-5 shadow m-3">
+        <h2 className="mb-5">Informasi Geografis</h2>
+        <div className="list-layanan ">
+          <InformasiGeografis />
+        </div>
+      </div>
+      <div className=" border rounded p-5 shadow m-3">
+        <h2 className="mb-5">Jadwal Pengangkutan</h2>
+        <div className="list-layanan ">
+          <JadwalPengangkutan />
+        </div>
+      </div>
+      <LaporPengaduanSampah />
     </div>
   );
 }
